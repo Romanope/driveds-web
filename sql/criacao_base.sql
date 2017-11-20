e58d5a828b5c27f2b6666bfc79a1d424dabbd5bb
Carlos Romano <carlosromanno@gmail.com>
2017-11-20 18:48:04 -0200

    CRIAÇÃO TABELA ARQUIVO

--- sql/criacao_base.sql
+++ sql/criacao_base.sql
@@ -24,7 +24,20 @@ primary key (COMP_ID)
 
 ) ENGINE=innoDB;
 
+create table ARQUIVO (
+
+ARQ_ID      integer NOT NULL AUTO_INCREMENT,
+USU_ID integer NOT NULL,
+FILE_NAME   VARCHAR(500) NOT NULL,
+ARQ_SYN    Bool NOT NULL DEFAULT 0,
+ARQ_REMOVIDO Bool NOT NULL DEFAULT 0,
+
+primary key (ARQ_ID)
+
+) ENGINE=innoDB;
+
 alter table driveds.COMPARTILHAMENTO add constraint fk_id_user_dono_file1 foreign key (USU_ID_PROP) references driveds.USUARIO (USU_ID);
 alter table driveds.COMPARTILHAMENTO add constraint fk_id_user_comp_file1 foreign key (USU_ID_COMP) references driveds.USUARIO (USU_ID);
+alter table driveds.ARQUIVO add constraint fk_id_user_arquivo foreign key (USU_ID) references driveds.USUARIO (USU_ID);
 
 commit;
